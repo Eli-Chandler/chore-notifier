3bd9b1169c7d0a0a912830465cdb3bfc877b836f
Eli Chandler <53414320+Eli-Chandler@users.noreply.github.com>
2026-01-11 01:21:42 +1300

    Feature/orval (#4)
    * add orval generation
    
    * make runnable
    
    * ignore the openapi spec in precommit
    
    * remove build time openapi generation

--- services/frontend/src/main.tsx
+++ services/frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from "react-router";
 import './index.css';
+import './config/axios';
 import App from "./App.tsx";
 import HomePage from "@/HomePage.tsx";
 import User from "@/User.tsx";
